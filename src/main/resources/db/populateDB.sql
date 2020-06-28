bcc937c3ed544943ca9c97f5c6ee0fd0f864683e
viktorma <viktor.masnyi@playtech.com>
2020-06-28 16:59:00 +0300

    HW3 main - final

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,10 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+  (100000, '2020-06-26 10:09:08.532425', 'test user meal', 111),
+  (100000, '2020-06-27 10:09:08.532425', 'test user meal', 222),
+  (100001, '2020-06-28 10:09:08.532425', 'test admin meal', 333);
+
+
